4af2ba11770d1e5baf1be43724b92f3b9b40794e
Dieggo Carrilho <dieggopsc@gmail.com>
2017-06-07 22:16:20 -0300

    contagem de downloads

--- resources/views/site/downloads.blade.php
+++ resources/views/site/downloads.blade.php
@@ -12,7 +12,7 @@
                     <div class="list-group">
                         @foreach($downloads as $download)
                             <a href="{!! route('downloads-one', $download->id) !!}" target="_blank" class="list-group-item">
-                                <span class="badge">{!! $download->quantidade !!}</span>
+                                <span class="badge">{!! $download->quantidade !!} downloads</span>
                                 <h4 class="list-group-item-heading"><strong>{!! $download->title !!}</strong></h4>
                                 <p class="list-group-item-text">{!! $download->sobre !!}</p>
 
