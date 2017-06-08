23954da8bf2f7d2764aa471bf2305af6dfa32a69
Dieggo Carrilho <dieggopsc@gmail.com>
2017-06-07 22:43:39 -0300

    create admins

--- app/Http/Controllers/AdminAuth/RegisterController.php
+++ app/Http/Controllers/AdminAuth/RegisterController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers\AdminAuth;
 
+use App\AdminUser;
 use App\User;
 use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Validator;
@@ -77,7 +78,7 @@ class RegisterController extends Controller
      */
     protected function create(array $data)
     {
-        return User::create([
+        return AdminUser::create([
             'name' => $data['name'],
             'email' => $data['email'],
             'password' => bcrypt($data['password']),
