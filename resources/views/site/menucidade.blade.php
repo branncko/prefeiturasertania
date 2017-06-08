ff88bbe872efaf63c800c31ade5d5b6714308237
Dieggo Carrilho <dieggopsc@gmail.com>
2017-06-07 22:09:53 -0300

    Menu lateral Cidade

--- resources/views/site/menucidade.blade.php
+++ resources/views/site/menucidade.blade.php
@@ -1,6 +1,6 @@
 <ul class="nav nav-tabs nav-stacked">
     <li><a href="{!! route('cidade') !!}">História</a></li>
-    <li><a href="#">Dados</a></li>
-    <li><a href="#">Hino</a></li>
-    <li><a href="#">Ex-Prefeitos</a></li>
+    <li><a href="{!! route('dados') !!}">Dados</a></li>
+    <li><a href="{!! route('hino') !!}">Hino</a></li>
+    <li><a href="{!! route('exprefeitos') !!}">Ex-Prefeitos</a></li>
 </ul>
