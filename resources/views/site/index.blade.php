792d1c9204a4e80949998347de5ea2f24dda945f
Dieggo Carrilho <dieggopsc@gmail.com>
2019-05-01 23:59:47 -0300

    link gecea

--- resources/views/site/index.blade.php
+++ resources/views/site/index.blade.php
@@ -52,6 +52,7 @@
                         <li><a href="{!! route('transparencia') !!}" alt="Portal da Tranparência"  target="_blank">Portal da Transparência</a></li>
                         <li><a href="https://www.tributosmunicipais.com.br/NFE-sertania/" alt="Nota Fiscal Eletrônica" target="_blank">Nota Fiscal Eletrônica</a></li>
                         <li><a href="http://gestor.tributosmunicipais.com.br/sertania/views/publico/portaldocontribuinte" alt="Portal do Contribuinte" target="_blank">Portal do Contribuinte</a></li>
+                        <li><a href="http://gecea.sertania.pe.gov.br" alt="Sistema GECEA" target="_blank">Sistema</a></li>
                         <li><a href="http://www.sertania.pe.gov.br/webmail" alt="WebMail" target="_blank">WebMail</a></li>
                     </ul>
 
