86d5ddc03fbd1eb12e4e7a90ca914505751faf58
Dieggo Carrilho <dieggopsc@gmail.com>
2020-12-12 13:44:24 -0300

    links

--- resources/views/site/adirblank.blade.php
+++ resources/views/site/adirblank.blade.php
@@ -29,12 +29,12 @@
                     </p>
                     <p>
                         <h2><strong>Chamada 02/2020</strong></h2>
-                        <p><a href="https://docs.google.com/forms/d/e/1FAIpQLSfHjdEHOoYSAlOn4wrVpAuyUbI8ppL6iWArDvdTkel2mhsZ1A/viewform" target="_Blank"> Cadastro Inciso III - Chamada Pública - 002/2020</a></p>
+                        <p><a href="https://forms.gle/yRghkPfpEgMPii1J7" target="_Blank"> Cadastro Inciso III - Chamada Pública - 002/2020</a></p>
                         <p><a href="http://sertania.pe.gov.br/downloads/101" target="_Blank"> Chamada Pública 003/2020 </a></p>
                     </p>
                     <p>
                         <h2><strong>Chamada 03/2020</strong></h2>
-                        <p><a href="https://docs.google.com/forms/d/e/1FAIpQLSfHjdEHOoYSAlOn4wrVpAuyUbI8ppL6iWArDvdTkel2mhsZ1A/viewform" target="_Blank"> Cadastro Inciso III - Chamada Pública - 003/2020</a></p>
+                        <p><a href="https://docs.google.com/forms/d/e/1FAIpQLScX7-EjxvVoaqtCd6lNy9fCXBNfJflwEg_ZQhmjnwBYO5uxdg/viewform" target="_Blank"> Cadastro Inciso III - Chamada Pública - 003/2020</a></p>
                         <p><a href="http://sertania.pe.gov.br/downloads/101" target="_Blank"> Chamada Pública 003/2020 </a></p>
                     </p>
 
